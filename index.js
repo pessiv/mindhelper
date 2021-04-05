001df1ec9c91c620f0c8011ab0f11399381099fa
Pessi Vilppolahti <pessiv@outlook.com>
2021-04-05 22:31:55 +0300

    added more functions to index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var ideas = require('./lib/ideas')
-var memories = require('./lib/accessdata')
 const express = require('express')
+var memory = require('./data/memory.json')
+var ideas = require('./data/ideas.json')
+const IdeaTonttu = require('./lib/ideas')
+let ideaTonttu = new IdeaTonttu(ideas)
+const DataTonttu = require('./lib/accessdata')
+let dataTonttu = new DataTonttu(memory)
 const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+app.get('/getideas',function(req,res){
+  res.send(ideaTonttu.getEverything())
+})
+app.get('/getmemory',function(req,res){
+  res.send(ideaTonttu.getEverything())
 })
 app.listen(3000)
